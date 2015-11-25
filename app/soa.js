a5f769cefb42ace98bacc2d28689aebc0b6f3438
mtorre4580 <mtorre4580@outlook.com>
2015-11-25 10:41:59 -0300

    ejemplo respuesta soa-comparer

--- app/soa.js
+++ app/soa.js
@@ -1,65 +1,73 @@
 module.exports = {
-	UAT: {
-		artefacto : "Actefacto 1",
-		particion : "Particion Prueba1",
-		version : '1.xx',
-		default : true,
-		fechaDespliegue : '16/11/15'	
-	},
-	IST: {
-		artefacto : "Actefacto 1",
-		particion : "Particion Prueba2",
-		version : '3.xx',
-		default : true,
-		fechaDespliegue : '15/11/15'	
-	},
-	QAM: {
-		artefacto : "",
-		particion : "Particion Prueba3",
-		version : '2.xx',
-		default : true,
-		fechaDespliegue : '13/11/15'	
-	},
-	PRODAR: {
-		artefacto : "Actefacto 1",
-		particion : "Particion Prueba4",
-		version : '12.xx',
-		default : true,
-		fechaDespliegue : '13/11/15'	
-	},
-	Ambiente5: {
-		artefacto : "Actefacto 12",
-		particion : "Particion Prueba5",
-		version : '1.xx',
-		default : true,
-		fechaDespliegue : '13/11/15'	
-	},
-	Ambiente6: {
-		artefacto : "Actefacto 11",
-		particion : "Particion Prueba6",
-		version : '11.x.x',
-		default : true,
-		fechaDespliegue : '13/11/15'	
-	},
-	Ambiente7: {
-		artefacto : "",
-		particion : "Particion Prueba7",
-		version : '2.13.x',
-		default : true,
-		fechaDespliegue : '13/11/15'	
-	},
-	Ambiente8: {
-		artefacto : "Actefacto 22",
-		particion : "Particion Prueba8",
-		version : '0.13.x',
-		default : true,
-		fechaDespliegue : '13/11/15'	
-	},
-	Ambiente9: {
-		artefacto : "Actefacto 51",
-		particion : "Particion Prueba9",
-		version : '0.0.1',
-		default : true,
-		fechaDespliegue : '13/11/15'	
-	}
+	"Ambiente 1" : [
+        {
+            "artefacto" : "Actefacto 1",
+            "particion" : "Particion Prueba1",
+            "version" : "1.x.x",
+            "default" : true,
+            "fechaDespliegue" : '16/11/15'
+        },
+        {
+            "artefacto" : "Actefacto 5",
+            "particion" : "Particion Prueba2",
+            "version" : "11.x.x",
+            "default" : true,
+            "fechaDespliegue" : '12/11/15'
+        },
+        {
+            "artefacto" : "Actefacto 12",
+            "particion" : "Particion Prueba5",
+            "version" : "3.x.x",
+            "default" : true,
+            "fechaDespliegue" : '19/11/15'
+        }
+    ],
+    "Ambiente 2": [
+		{
+            "artefacto" : "Actefacto 1",
+            "particion" : "Particion Prueba6",
+            "version" : "0.x.x",
+            "default" : true,
+            "fechaDespliegue" : '15/11/15'
+        },
+        {
+            "artefacto" : "Actefacto 5",
+            "particion" : "Particion Prueba20",
+            "version" : "1.1.x",
+            "default" : true,
+            "fechaDespliegue" : '25/8/15'
+        }
+    ],
+    "Ambiente 3": [
+		{
+            "artefacto" : "Actefacto 11",
+            "particion" : "Particion Prueba33",
+            "version" : "0.3.x",
+            "default" : true,
+            "fechaDespliegue" : '1/10/15'
+        },
+        {
+            "artefacto" : "Actefacto 5",
+            "particion" : "Particion Prueba1",
+            "version" : "3.1.x",
+            "default" : true,
+            "fechaDespliegue" : '5/3/15'
+        }
+    ],
+    "Ambiente 4": [
+		{
+            "artefacto" : "Actefacto 12",
+            "particion" : "Particion Prueba0",
+            "version" : "1.3.1",
+            "default" : true,
+            "fechaDespliegue" : '1/5/15'
+        },
+        {
+            "artefacto" : "Actefacto 5",
+            "particion" : "Particion Prueba1",
+            "version" : "1.1.x",
+            "default" : true,
+            "fechaDespliegue" : '1/2/15'
+        }
+    ]
 }
