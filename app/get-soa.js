c601bbe40921e8a1c5634a4fa843e30286ff8858
mtorre4580 <mtorre4580@outlook.com>
2015-12-03 14:06:20 -0300

    view soa comparer, ejecutar comando

--- app/get-soa.js
+++ app/get-soa.js
@@ -1,8 +1,40 @@
 var app = require('express')();
-var soa = require('./soa'); 
+var child_process = require('child_process');
+var soa = '';
+var method_override = require('method-override');
 
-app.get('/soa-comparer', function (req, res) {
-   res.json(soa).end();
+app.use(method_override());
+app.get('/soa-comparer/:ambiente?', function (req, res) {
+	ejecutarComando('IST'); // el archivo txt, tiene el nombre del ambiente, esto no va, sacar despues....
+  	res.json(soa).end();	
 });
+
+function ejecutarComando(ambiente){ // el comando tiene q recibir el ambiente...
+	child_process.exec('cd /home/mtorres/proto-envman/appTest/ && node app.js',{maxBuffer: 1024*1024},function(error, stdout, stderr){
+		var ouput = stdout.toString();
+		soa = ouput.substring('[INICIO]'.length,ouput.lastIndexOf('[FIN]'));
+		if(error){
+			console.log('error al ejecutar el comando ' + error);
+		}
+	});
+}
+
+// todavia no se testio, si recibe por parametro el ambiente, ejecuta el script con ese ambiente, si viene null, ejecuta un script por ambiente...
+
+/*
+var tabla = {};
+app.use(method_override());
+app.get('/soa-comparer/:ambiente?', function (req, res) {
+	if(req.params.ambiente){
+		var artefactos = ejecutarComando(req.params.ambiente);
+	}else{
+		for(var index in window.ambientes){	//FIXME--> verificar si se puede obtener la variable de este lado...
+			var artefactos = this.ejecutarComando(window.ambientes[index]);
+		}
+	}
+	res.json(tabla).end();
+});*/
+
+
 
 module.exports = app;
