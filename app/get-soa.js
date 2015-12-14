ee6f3204d62fee37b1d5aaaec2706703fdf91317
mtorre4580 <mtorre4580@outlook.com>
2015-12-14 13:32:42 -0300

    se agrega servicio por post, soa-comparer

--- app/get-soa.js
+++ app/get-soa.js
@@ -1,40 +1,39 @@
 var app = require('express')();
 var child_process = require('child_process');
-var soa = require('./soa.js');
-var method_override = require('method-override');
 var config = require('./config'); // se agrega el archivo config.js , para obtener los ambientes
-
+var bodyParser = require('body-parser');
+var data = {};
 this.artefactos = ''; 
 
-app.use(method_override());
+app.use(bodyParser({ extended : false}));
+app.use(bodyParser.json());
 
-// si la url se le agrega un parametro , ejecuta el comando para ese ambiente , ejemplo -> /soa-comparer?ambiente=DESA
-// si la url viene sin parametro, ejecuta el comando para todos los ambientes
+// GET --> muestra todos los ambientes
 app.get('/soa-comparer', function (req, res) { 
-  // Comentado para que funcione la pantalla
-/*	var ambiente = req.query.ambiente;
-	console.log('ambiente : ' + ambiente);
-	if(ambiente){
-		crearObjectSoa(ambiente);
-	}else{
-		var ambientes = Object.keys(config.ambientes);
-		for(var index in ambientes){	
-			crearObjectSoa(ambientes[index]);
-		}
-	}*/
-  	res.json(soa).end();	
+	var ambientes = Object.keys(config.ambientes);
+	for(var index in ambientes){	
+		crearObjectSoa(ambientes[index]);
+	}
+  	res.json(data).end();	
+});
+
+// POST --> muestra los ambientes que recibe en el vector pasado por parametro (ambientes)
+app.post('/soa-comparer', function (req, res) {
+	for(var index in req.body.ambientes){	
+		crearObjectSoa(req.body.ambientes[index]);
+	}
+	res.json(data).end();
 });
 
 function crearObjectSoa(ambiente){
-	var data = [];
-	soa[ambiente] = [];
-	soa[ambiente] = ejecutarComando(ambiente);
+	data[ambiente] = [];
+	data[ambiente] = ejecutarComando(ambiente);
 }
 
 function ejecutarComando(ambiente){ 
 	var self = this;
 	// cuando este la app de python , agregar en el exec el param ambiente recibido...
-	child_process.exec('cd /home/mtorres/proto-envman/appTest/ && node app.js',{maxBuffer: 1024*1024},function(error, stdout, stderr){
+	child_process.exec('cd appTest && node app.js',{maxBuffer: 1024*1024},function(error, stdout, stderr){
 		var stdoutString = stdout.toString();
 		self.artefactos  = stdoutString.substring('[INICIO]'.length,stdoutString.lastIndexOf('[FIN]'));
 		self.artefactos  = JSON.parse(self.artefactos);
--- public/js/views/soa-comparer-v2.js
+++ public/js/views/soa-comparer-v2.js
@@ -1,38 +1,16 @@
 EnvMan.Views.SOAComparer = Backbone.View.extend({
 	
 	initialize : function(){
-	this.template = swig.compile(getTemplate('templates/soa-screen-v2.html'));
-    this.artefactos = {};
-    this.arrayArtefactos = [];
-    var self = this;
-		$.ajax({
-			url : '/soa-comparer',
-			method : 'GET',
-			async : false,
-			contentType : 'application/json',
-			success : function (data) { 
-				console.log('consulta exitosa servicio -> /soa-comparer');
-				for(var ambiente in data){
-					for(var index in data[ambiente]){
-						var artefacto = data[ambiente][index].artefacto ;
-						if(!self.artefactos[artefacto]){
-							self.artefactos[artefacto] = {};
-							self.artefactos[artefacto].particion = data[ambiente][index].particion;
-							self.artefactos[artefacto].ambientes = {} ;
-							for(var index2 in window.ambientes){
-								self.artefactos[artefacto].ambientes[window.ambientes[index2]] = {};
-							}
-						}
-						self.artefactos[artefacto].ambientes[ambiente] = {
-							version : data[ambiente][index].version,
-							fecha: data[ambiente][index].fechaDespliegue,
-							diff: data[ambiente][index].diff 
-						};
-					}
-				}
-       		self.ordenarArtefactos('artefacto');
-			}
-		});
+		this.template = swig.compile(getTemplate('templates/soa-screen-v2.html'));
+	    this.artefactos = {};
+	    this.arrayArtefactos = [];
+		this.ajaxGetSoa(); 
+
+		//ejemplo por POST
+		
+		/*var obj = {};
+		obj.ambientes = ['UAT','IST'];
+		this.ajaxPostSoa(obj);*/
 	},
 
 	events : {
@@ -42,9 +20,7 @@ EnvMan.Views.SOAComparer = Backbone.View.extend({
 	},
 
   ordenar : function (e) {
-
     var campo = $(e.target).attr('sort-field');
-
     this.ordenarArtefactos(campo);
     this.render();
   },
@@ -83,18 +59,66 @@ EnvMan.Views.SOAComparer = Backbone.View.extend({
     });
   },
 
-	render:function(){
-		this.$el.html(this.template({
-			artefactos : this.arrayArtefactos 
-		}));
-	},
-	
-	save: function(e){
-		console.log('evento guardar...');
-	},
+  ajaxGetSoa:function(){
+  	var self = this;
+	$.ajax({
+		url : '/soa-comparer',
+		method : 'GET',
+		async : false,
+		contentType : 'application/json',
+		success : function (data) { 
+       		self.formatearData(data);
+		}
+	});
+  },
 
-	cancel:function(e){
-		console.log('evento cancelar...');
+  ajaxPostSoa: function(ambientes){
+  	var self = this;
+  	$.ajax({
+		url : '/soa-comparer', 
+	    method : 'POST',
+	    contentType : 'application/json',
+	    data : JSON.stringify(ambientes),
+	    success: function (data) {
+	     	self.formatearData(data);
+		}
+	});
+  },
+
+  formatearData:function(data){
+  	for(var ambiente in data){
+		for(var index in data[ambiente]){
+			var artefacto = data[ambiente][index].artefacto ;
+			if(!this.artefactos[artefacto]){
+				this.artefactos[artefacto] = {};
+				this.artefactos[artefacto].particion = data[ambiente][index].particion;
+				this.artefactos[artefacto].ambientes = {} ;
+				for(var index2 in window.ambientes){
+					this.artefactos[artefacto].ambientes[window.ambientes[index2]] = {};
+				}
+			}
+			this.artefactos[artefacto].ambientes[ambiente] = {
+				version : data[ambiente][index].version,
+				fecha: data[ambiente][index].fechaDespliegue,
+				diff: data[ambiente][index].diff 
+			};
+		}
 	}
+    this.ordenarArtefactos('artefacto');
+  },
+
+  render:function(){
+	this.$el.html(this.template({
+		artefactos : this.arrayArtefactos 
+	}));
+  },
+
+  save: function(e){
+	console.log('evento guardar...');
+  },
+
+  cancel:function(e){
+	console.log('evento cancelar...');
+  }
 
 });
